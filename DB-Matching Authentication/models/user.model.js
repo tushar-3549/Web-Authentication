bb46e50e724ea56b29e1b879a7c49187fa99210d
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-10-22 11:13:03 +0600

    Add files via upload

---
+++ DB-Matching Authentication/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+
+module.exports = mongoose.model("user", userSchema);
